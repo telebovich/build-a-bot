25f89bca0b9fb2c7b656588484f2f91ca905c371
Robert Lebovich <robert.lebovich@gmail.com>
2023-02-03 16:12:48 +0200

    Setup

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo" />
+          Build-a-Bot
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <HomePage />
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import HomePage from './components/HomePage.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
+    HomePage,
   },
 });
 </script>
 
 <style lang="scss">
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" aria-hidden="true" alt="robot" />
+    </div>
+    <div>
+      <a href="">Get started</a> building your first robot
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.home {
+  text-align: center;
+}
+
+.robot {
+  height: 300px;
+}
+</style>
