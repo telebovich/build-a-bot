d8cd9e7bd330168291f1f73c85f3239943c60756
Robert Lebovich <robert.lebovich@gmail.com>
2023-03-03 16:38:38 +0200

    Adds title above robot's head

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="top-row">
       <div class="top part">
+        <div class="robot-name">{{ selectedRobot.head.title }}</div>
         <img :src="selectedRobot.head.src" title="head" alt="head" />
         <button @click="selectPreviousHead()" class="prev-selector">&#9668;</button>
         <button @click="selectNextHead()" class="next-selector">&#9658;</button>
@@ -255,5 +256,12 @@ export default defineComponent({
 
 .right .next-selector {
   right: -3px;
+}
+
+.robot-name {
+  position: absolute;
+  top: -25px;
+  text-align: center;
+  width: 100%;
 }
 </style>
