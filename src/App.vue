2a17c76caed0b95c530ff3f52a20402513bdc2a6
Tzvi Lebovich <tzvi@200apps.co>
2023-10-17 11:26:57 +0300

    Styling Links based on the Active Route

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,20 @@
     <nav>
       <ul>
         <li class="nav-item">
-          <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo" />
-          Build-a-Bot
+          <router-link :to="{ name: 'Home' }" class="nav-link" active-class="home-active">
+            <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo" />
+            Build-a-Bot
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Build' }" class="nav-link">
+            Build
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Search' }" class="nav-link">
+            Search
+          </router-link>
         </li>
       </ul>
     </nav>
@@ -55,6 +67,11 @@ ul {
   display: flex;
 }
 
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+
 .nav-item {
   display: inline-block;
   padding: 5px 10px;
@@ -65,5 +82,14 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+
+.router-link-active {
+  color: white;
+}
+
+.home-active {
+  color: white;
+  text-shadow: 2px 2px black;
 }
 </style>
