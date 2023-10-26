245456abfaefbd52beda3c7c70302a421d214c3f
Tzvi Lebovich <tzvi@200apps.co>
2023-10-26 21:30:14 +0300

    Changing the Router History Mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 import SearchComponent from '../search/SearchComponent.vue';
 import PartInfo from "@/parts/PartInfo.vue";
 
 export default createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
